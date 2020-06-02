4fe7796b8d72607c75e701c9d96efeeee4f96065
Michal Terranova <michalterranova@mail.com>
2020-06-02 15:40:16 -0400

    all files

---
+++ config/PersistenceConfiguration.java
@@ -0,0 +1,18 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("");
+//        System.out.println("My custom bean has been initialized and set");
+//        return builder.build();
+//    }
+}
